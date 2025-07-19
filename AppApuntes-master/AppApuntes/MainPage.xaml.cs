05f1fb8469007adbef8920776a91d9acb73983ea
WilsonEgassoftware <146997073+WilsonEgassoftware@users.noreply.github.com>
2025-07-19 00:57:15 -0500

    Update MainPage.xaml.cs
    Cambios finales

--- AppApuntes-master/AppApuntes/MainPage.xaml.cs
+++ AppApuntes-master/AppApuntes/MainPage.xaml.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections.ObjectModel;
 
 namespace AppApuntes;
-
+//definición de la clase mainpage
 public partial class MainPage : ContentPage
-{
+{ //propiedad que contiene varios objetos de la clase Nota 
     public ObservableCollection<Nota> listaNotas { get; set; }
 
     public MainPage()
@@ -16,8 +16,8 @@ public partial class MainPage : ContentPage
 
     public class Nota
     {
-        public string Texto { get; set; }
-        public string Fecha { get; set; }
+        public string Texto { get; set; }// contenido 
+        public string Fecha { get; set; }// fecha de registro de la nota 
     }
 
     private async void Agregar_Clicked(object sender, EventArgs e)
@@ -31,9 +31,9 @@ public partial class MainPage : ContentPage
     }
 
     private async void NotasCollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
+    {// verificamos si hay jna Nota seleccionada 
         if (e.CurrentSelection.FirstOrDefault() is Nota notaSeleccionada)
-        {
+        { // Dejamos de seleccionar el elemento para permitir nuevas selecciones de notas 
             NotasCollectionView.SelectedItem = null;
             await Navigation.PushAsync(new NotaPage(listaNotas, notaSeleccionada));
         }
