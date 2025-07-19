0e511d18942726dd613c7b033c757d86260ff6e2
WilsonEgassoftware <146997073+WilsonEgassoftware@users.noreply.github.com>
2025-07-19 01:18:41 -0500

    Update MauiProgram.cs

--- AppApuntes-master/AppApuntes/MauiProgram.cs
+++ AppApuntes-master/AppApuntes/MauiProgram.cs
@@ -5,11 +5,11 @@ namespace AppApuntes
     public static class MauiProgram
     {
         public static MauiApp CreateMauiApp()
-        {
+        { // creamos el constructor para configurar la app
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
+                .ConfigureFonts(fonts =>// configurar las funciones del programa 
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
